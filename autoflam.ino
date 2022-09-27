c785298b4999dc0d929220a3092750d281e81a8a
rjkiv <76180273+rjkiv@users.noreply.github.com>
2022-09-27 09:42:48 -0400

    autoflam ported to arduino!

--- autoflam.ino
+++ autoflam.ino
@@ -4,32 +4,48 @@
 #include <MIDI.h>
 
 AsyncTimer t;
-unsigned long lastCymbal = millis();
-unsigned int flamAmt = 15;
+unsigned long lastCymbal = 0;
+unsigned long flamAmt = 15;
 const int cymbal_notes[] = {22, 26, 42, 46, 54, 51, 53, 56, 59, 49, 52, 55, 57, 41, 43};
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-// per bookreader's js - CHECK THIS EVERY TIME POLLING THE MIDI INPUT PICKS UP A NOTE:
-// if note[2] (or channel) > 0 and note[0] (or pitch) >= 144 and note[0] (or pitch) <= 159):
-//    if the length of note[] is 3 (?) and note[1] (or velocity) is found in cymbal_notes:
-//        if millis() - lastCymbal < flamAmt:
-//              delay sending the note out via setTimeOut
-//        else:
-//              lastCymbal = millis()
-//              send the note out
-//    else:
-//        send the note out
+bool note_in_cymbal_notes(int note){
+  for(int i = 0; i < 15; i++){
+    if(note == cymbal_notes[i]) return true;
+  }
+  return false;
+}
+
+void sendNote(byte channel, byte pitch, byte velocity, bool on){
+  if(on) MIDI.sendNoteOn(pitch, velocity, channel);
+  else MIDI.sendNoteOff(pitch, velocity, channel);
+}
+
+void handleDrumNote(byte channel, byte pitch, byte velocity, bool on){
+  digitalWrite(LED_BUILTIN, on);
+  if(int(channel) > 0 && int(pitch) >= 144 && int(pitch) <= 159){
+    if(note_in_cymbal_notes(int(velocity))){
+      if(millis() - lastCymbal < flamAmt){
+         //send the note out delayed
+         t.setTimeout([=]() { sendNote(channel, pitch, velocity, on); }, flamAmt);
+      }
+      else{
+        lastCymbal = millis();
+        sendNote(channel, pitch, velocity, on);
+      }
+    }
+    else sendNote(channel, pitch, velocity, on);
+  }
+  
+}
 
 
 void handleNoteOn(byte channel, byte pitch, byte velocity){
-//  printNote(int(channel), int(pitch), int(velocity));
-  digitalWrite(LED_BUILTIN, HIGH);
-  MIDI.sendNoteOn(pitch, velocity, channel);
+  handleDrumNote(channel, pitch, velocity, true);
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity){
-  digitalWrite(LED_BUILTIN, LOW);
-  MIDI.sendNoteOff(pitch, velocity, channel);
+  handleDrumNote(channel, pitch, velocity, false);
 }
 
 // setup code, runs once
@@ -39,11 +55,10 @@ void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
-//  t.setInterval([]() { digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); }, 1000);
 }
 
 // main code, runs repeatedly
 void loop() {
-//  t.handle();
+  t.handle();
   MIDI.read();
 }
