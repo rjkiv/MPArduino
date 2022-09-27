3e9781b7743fe084477a8bc837c2892387030d32
rjkiv <76180273+rjkiv@users.noreply.github.com>
2022-09-26 21:08:05 -0400

    bookreader pseudocode

--- autoflam.ino
+++ autoflam.ino
@@ -4,7 +4,22 @@
 #include <MIDI.h>
 
 AsyncTimer t;
+unsigned long lastCymbal = millis();
+unsigned int flamAmt = 15;
+const int cymbal_notes[] = {22, 26, 42, 46, 54, 51, 53, 56, 59, 49, 52, 55, 57, 41, 43};
 MIDI_CREATE_DEFAULT_INSTANCE();
+
+// per bookreader's js - CHECK THIS EVERY TIME POLLING THE MIDI INPUT PICKS UP A NOTE:
+// if note[2] (or channel) > 0 and note[0] (or pitch) >= 144 and note[0] (or pitch) <= 159):
+//    if the length of note[] is 3 (?) and note[1] (or velocity) is found in cymbal_notes:
+//        if millis() - lastCymbal < flamAmt:
+//              delay sending the note out via setTimeOut
+//        else:
+//              lastCymbal = millis()
+//              send the note out
+//    else:
+//        send the note out
+
 
 void handleNoteOn(byte channel, byte pitch, byte velocity){
 //  printNote(int(channel), int(pitch), int(velocity));
