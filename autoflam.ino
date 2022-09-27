d1102d047fd4e88a92c79cc585b826e6cf2858d2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2022-09-26 20:44:09 -0400

    file can now read MIDI input

--- autoflam.ino
+++ autoflam.ino
@@ -1,17 +1,45 @@
 // Listens for drum inputs, and if two cymbals are detected,
 // will automatically delay one cymbal to prevent the MPA glitch.
 #include <AsyncTimer.h>
+#include <MIDI.h>
 
 AsyncTimer t;
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void printNote(byte channel, byte pitch, byte velocity){
+  Serial.print("[");
+  Serial.print(channel);
+  Serial.print(", ");
+  Serial.print(pitch);
+  Serial.print(", ");
+  Serial.print(velocity);
+  Serial.print("]\n");
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity){
+//  printNote(int(channel), int(pitch), int(velocity));
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity){
+  digitalWrite(LED_BUILTIN, LOW);
+}
 
 // setup code, runs once
 void setup() {
+//  Serial.begin(115200);
+//  while(!Serial);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  t.setInterval([]() { digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); }, 1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+//  Serial.println("Ready to play.");
+//  t.setInterval([]() { digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); }, 1000);
 }
 
 // main code, runs repeatedly
 void loop() {
-  t.handle();
+//  t.handle();
+  MIDI.read();
 }
